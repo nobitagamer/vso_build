b54f38c94f5df72c2bb5c4a9d4d830cb73ad33ce
Nguyen Khac Trieu <trieunk@yahoo.com>
2017-04-17 22:31:10 +0700

    feat: Support GitLab Merge Request Hook event
    + Only trigger on request created or new code pushed to merge request.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-module['exports'] = function vso_build(hook) {
+module['exports'] = function vso_build (hook) {
   var params = hook.params
-  if (!params.pullrequest) {
+  var sourceBranch = ''
+
+  if (hook.req.headers['x-gitlab-event'] === 'Merge Request Hook') {
+    // GitLab
+    var action = params.object_attributes.action
+    if (action === 'open' || (action === 'update' && params.object_attributes.oldrev !== '')) {
+      sourceBranch = params.object_attributes.source_branch
+    } else {
+      hook.res.end('Skipped!')
+    }
+  } else if (hook.req.headers['x-event-key'] === 'pullrequest:created' || hook.req.headers['x-event-key'] === 'pullrequest:updated') {
+    // Bitbucket
+    sourceBranch = params.pullrequest.source.branch.name
+  } else {
     hook.res.end('Bad request source!')
   }
+
+  console.log(`Building: ${sourceBranch}`)
   
   var url = `https://${params.account}.visualstudio.com/DefaultCollection/${params.project}/_apis/build/builds?api-version=2.0`
   var options = {
@@ -12,15 +27,15 @@ module['exports'] = function vso_build(hook) {
       'definition': {
         'id': params.id
       },
-      'sourceBranch': `refs/heads/${params.pullrequest.source.branch.name}`
+      'sourceBranch': `refs/heads/${sourceBranch}`
     }
   }
 
-  var client = hook.sdk.createClient({});
-  client.keys.checkAccess('hook::logs::read', function(err, h){
+  var client = hook.sdk.createClient({})
+  client.keys.checkAccess('hook::logs::read', function (err, h) {
     console.log(url)
     console.log(options)
-  });
+  })
 
   // npm modules available, see: http://hook.io/modules
   var request = require('request')
